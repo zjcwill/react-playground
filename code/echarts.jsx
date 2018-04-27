a2d4f709e8a01127f9defb18ce7ef561a27915ef
Nyle.Zhang <zjcwill@foxmail.com>
2018-04-27 21:56:34 +0800

    ADD babel

--- code/echarts.jsx
+++ code/echarts.jsx
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
-import echarts from 'echarts';
-
+import React, { Component } from "react";
+import echarts from "echarts";
 
 class EchartsCom extends Component {
-    componentDidMount(){
-       const myChart = echarts.init(document.getElementById('main'));
-       myChart.setOption({
-        title: { text: 'ECharts 入门示例' },
-        tooltip: {},
-        xAxis: {
-            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-        },
-        yAxis: {},
-        series: [{
-            name: '销量',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
-        }]
+  componentDidMount() {
+    const myChart = echarts.init(document.getElementById("main"));
+    myChart.setOption({
+      title: { text: "ECharts 入门示例" },
+      tooltip: {},
+      xAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "销量",
+          type: "bar",
+          data: [5, 20, 36, 10, 10, 20]
+        }
+      ]
     });
-    }
-    render() {
-        return (
-            <div id='main' style={{width:400,height:400}}>    
-            </div>
-        );
-    }
+  }
+  render() {
+    return <div id="main" style={{ width: 400, height: 400 }} />;
+  }
 }
 
 export default EchartsCom;
